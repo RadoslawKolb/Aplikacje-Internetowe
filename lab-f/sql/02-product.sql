6bb22ade94e49407fa004d96e0b24d56f7d7be46
Radosław Kolb <kr53914>
2025-01-23 08:42:31 +0100

    xd'

--- lab-f/sql/02-product.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE products (
-                          id INTEGER PRIMARY KEY AUTOINCREMENT,
-                          name TEXT NOT NULL,
-                          price REAL NOT NULL,
-                          manufacturer TEXT NOT NULL,
-                          production_date DATE NOT NULL
-);
--- lab-f/sql/03-comment.sql
+++
@@ -1,8 +0,0 @@
-create table comment
-(
-    id      integer not null
-        constraint comment_pk
-            primary key autoincrement,
-    author  text not null,
-    content text not null
-);
